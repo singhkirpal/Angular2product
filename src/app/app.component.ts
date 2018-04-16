5d39344d4a7afa8afa18faea0b99e2e3c68026a1
singhkirpal <kirpal997@gmail.com>
2018-04-16 23:33:24 +0530

    Completed initial changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'pm-root',
+  template:
+  '<div><h1>{{pageTitle}}</h1><div><pm-products></pm-products></div></div>'
+})
+ export class AppComponent{
+   pageTitle: string ='Kirk';
+ }
